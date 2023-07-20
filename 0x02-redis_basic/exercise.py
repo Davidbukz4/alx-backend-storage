6858f6353cad2ff54b823a80b0f1c24240fb602d
David Egwuatu <davidegwuatu2@gmail.com>
2023-07-20 04:27:08 +0000

    added task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,8 +4,10 @@ Redis basics
 '''
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
+
+#def count_calls(methods: Callable) -> Callable:
 
 class Cache:
     ''' Cache class '''
@@ -20,3 +22,23 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        ''' converts the data back to the desired format '''
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, value: bytes) -> str:
+        ''' get str from cache '''
+        return str(value.decode('utf-8'))
+
+    def get_int(self, value: bytes) -> int:
+        ''' get int from the cache '''
+        try:
+            value = int(value.decode('utf-8'))
+        except Exception:
+            value = 0
+        return value
